88f5e34c6fffb0c351f5f8f40fd7bed42c35f5fe
Amulya Kumar Sahoo <amulya349@gmail.com>
2015-11-11 11:16:33 +0530

    added views

--- server/app.js
+++ server/app.js
@@ -4,8 +4,9 @@ var port     = process.env.PORT || 8880;
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.set('views', __dirname + '../views');
-app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 // launch ======================================================================
 app.listen(port);
@@ -14,6 +15,5 @@ console.log('Listening at http://localhost:' + port);
 // routes here =================================================================
 
 app.get('/', function(req, res) {
-	// res.sendFile('index.html', {"root": __dirname});
-	res.render('index')
+	res.render('index.html')
 })
